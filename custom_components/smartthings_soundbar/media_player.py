3160184ddc8ba5d5aa563e443daf89482377754d
mimale <54315734+mimale@users.noreply.github.com>
2022-11-28 01:23:25 +0100

    Update media_player.py

--- custom_components/smartthings_soundbar/media_player.py
+++ custom_components/smartthings_soundbar/media_player.py
@@ -106,6 +106,9 @@ class SmartThingsSoundbarMediaPlayer(MediaPlayerEntity):
     def select_source(self, source, cmdtype="selectsource"):
         SoundbarApi.send_command(self, source, cmdtype)
 
+    def select_sound_mode(self, source, cmdtype="selectsoundmode"):
+        SoundbarApi.send_command(self, source, cmdtype)
+
     @property
     def device_class(self):
         return DEVICE_CLASS_SPEAKER
