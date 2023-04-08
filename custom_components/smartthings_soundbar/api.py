ac15720805baff7fbe086de8ccea42b89d6c3623
thierry-rhone <65720627+thierry-rhone@users.noreply.github.com>
2023-04-08 13:34:17 +0200

    Add  SoundbarApiSwitch

--- custom_components/smartthings_soundbar/api.py
+++ custom_components/smartthings_soundbar/api.py
@@ -40,6 +40,10 @@ class SoundbarApi:
         device_all_sources = json.loads(data['main']['supportedInputSources']['value'])
         device_muted = data['main']['mute']['value'] != "unmuted"
 
+        sourceFile = open('HOUBA.txt', 'w')
+        print(data, file = sourceFile)
+        sourceFile.close()
+
         if switch_state == "on":
             if device_source in CONTROLLABLE_SOURCES:
                 if playback_state == "playing":
@@ -60,17 +64,27 @@ class SoundbarApi:
             self._media_title = data['main']['trackDescription']['value']
         else:
             self._media_title = None
-
+            
         API_DEVICE_STATUS = API_DEVICE + "/components/main/capabilities/execute/status"
         API_FULL = "{'commands':[{'component': 'main','capability': 'execute','command': 'execute', 'arguments': ['/sec/networkaudio/soundmode']}]}"
         cmdurl = requests.post(API_COMMAND, data=API_FULL, headers=REQUEST_HEADERS)
         resp = requests.get(API_DEVICE_STATUS, headers=REQUEST_HEADERS)
         data = resp.json()
+        
+        sourceFile = open('HOUBA.txt', 'w')
+        print(data, file = sourceFile)
+        sourceFile.close()
+        
  
         device_soundmode = data['data']['value']['payload']['x.com.samsung.networkaudio.soundmode']
         device_soundmode_list = data['data']['value']['payload']['x.com.samsung.networkaudio.supportedSoundmode']
         self._sound_mode = device_soundmode
         self._sound_mode_list = device_soundmode_list
+        
+        sourceFile = open('HOUBA.txt', 'w')
+        print(device_soundmode, file = sourceFile)
+        sourceFile.close()
+         
     
 
     @staticmethod
@@ -121,3 +135,79 @@ class SoundbarApi:
             cmdurl = requests.post(API_COMMAND, data=API_FULL, headers=REQUEST_HEADERS)
         
         self.async_schedule_update_ha_state()
+
+class SoundbarApiSwitch:
+
+    @staticmethod
+    def device_update(self):
+    
+        API_KEY = self._api_key
+        REQUEST_HEADERS = {"Authorization": "Bearer " + API_KEY}
+        DEVICE_ID = self._device_id
+        API_DEVICE = API_DEVICES + DEVICE_ID
+        API_DEVICE_STATUS = API_DEVICE + "/components/main/capabilities/execute/status"
+        API_COMMAND = API_DEVICE + "/commands"
+        API_FULL = "{'commands':[{'component': 'main','capability': 'execute','command': 'execute', 'arguments': ['/sec/networkaudio/advancedaudio']}]}"
+        cmdurl = requests.post(API_COMMAND, data=API_FULL, headers=REQUEST_HEADERS)
+        resp = requests.get(API_DEVICE_STATUS, headers=REQUEST_HEADERS)
+        data = resp.json()
+
+        if self._name == "night_mode":
+            if data['data']['value']['payload']['x.com.samsung.networkaudio.nightmode'] == 1:
+                self._state = STATE_ON
+            else:
+                self._state = STATE_OFF
+        elif self._name == "bass_boost":
+            if data['data']['value']['payload']['x.com.samsung.networkaudio.bassboost'] == 1:
+                self._state = STATE_ON
+            else:
+                self._state = STATE_OFF
+        elif self._name == "voice_amplifier":
+            if data['data']['value']['payload']['x.com.samsung.networkaudio.voiceamplifier'] == 1:
+                self._state = STATE_ON
+            else:
+                self._state = STATE_OFF
+
+    @staticmethod
+    def send_command(self, argument, cmdtype):
+        API_KEY = self._api_key
+        REQUEST_HEADERS = {"Authorization": "Bearer " + API_KEY}
+        DEVICE_ID = self._device_id
+        API_DEVICES = API_BASEURL + "/devices/"
+        API_DEVICE = API_DEVICES + DEVICE_ID
+        API_COMMAND = API_DEVICE + "/commands"
+        API_COMMAND_DATA = "{'commands':[{'component': 'main','capability': 'execute','command': 'execute','arguments': ['/sec/networkaudio/advancedaudio',"
+        
+        if self._name == "night_mode": 
+            if cmdtype == "switch_off":  # turns off nightmode
+                API_COMMAND_ARG =  "{'x.com.samsung.networkaudio.nightmode': 0 }]}]}"
+                API_FULL = API_COMMAND_DATA + API_COMMAND_ARG
+                cmdurl = requests.post(API_COMMAND, data=API_FULL, headers=REQUEST_HEADERS)
+            elif cmdtype == "switch_on":  # turns on nightmode
+                API_COMMAND_ARG =  "{'x.com.samsung.networkaudio.nightmode': 1 }]}]}"
+                API_FULL = API_COMMAND_DATA + API_COMMAND_ARG
+                cmdurl = requests.post(API_COMMAND, data=API_FULL, headers=REQUEST_HEADERS)
+        elif self._name == "bass_boost": 
+            if cmdtype == "switch_off":  # turns off bassboost
+                API_COMMAND_ARG =  "{'x.com.samsung.networkaudio.bassboost': 0 }]}]}"
+                API_FULL = API_COMMAND_DATA + API_COMMAND_ARG
+                cmdurl = requests.post(API_COMMAND, data=API_FULL, headers=REQUEST_HEADERS)
+            elif cmdtype == "switch_on":  # turns on bassboost
+                API_COMMAND_ARG =  "{'x.com.samsung.networkaudio.bassboost': 1 }]}]}"
+                API_FULL = API_COMMAND_DATA + API_COMMAND_ARG
+                cmdurl = requests.post(API_COMMAND, data=API_FULL, headers=REQUEST_HEADERS)
+        elif self._name == "voice_amplifier": 
+            if cmdtype == "switch_off":  # turns off voiceamplifier
+                API_COMMAND_ARG =  "{'x.com.samsung.networkaudio.voiceamplifier': 0 }]}]}"
+                API_FULL = API_COMMAND_DATA + API_COMMAND_ARG
+                cmdurl = requests.post(API_COMMAND, data=API_FULL, headers=REQUEST_HEADERS)
+            elif cmdtype == "switch_on":  # turns on voiceamplifier
+                API_COMMAND_ARG =  "{'x.com.samsung.networkaudio.voiceamplifier': 1 }]}]}"
+                API_FULL = API_COMMAND_DATA + API_COMMAND_ARG
+                cmdurl = requests.post(API_COMMAND, data=API_FULL, headers=REQUEST_HEADERS)
+
+        self.async_schedule_update_ha_state()
+
+#        sourceFile = open('HOUBA.txt', 'w')
+#        print(nightmode_state, file = sourceFile)
+#        sourceFile.close()
